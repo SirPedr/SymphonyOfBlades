450a5bf5d90f480582fba98570d8bcd8507c599c
Pedro Paulo <ppagbpedro@gmail.com>
2017-11-05 20:20:18 -0200

    Update cenarioseEscolhas.js

--- js/cenarioseEscolhas.js
+++ js/cenarioseEscolhas.js
@@ -20,7 +20,7 @@ function campanha(){
   atualizaHUD(jogador);
 
   //Chama a função principal, apenas uma única vez
-  gerenciaCenarioseOpcoes('fimDeJogo', jogador);
+  gerenciaCenarioseOpcoes('tavernaInicial', jogador);
 
 }
 
